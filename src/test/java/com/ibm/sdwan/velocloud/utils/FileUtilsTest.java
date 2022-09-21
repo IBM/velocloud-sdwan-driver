37bf18289f49be830fe395ac2fe8a7b7cf159873
Siddalasha <guduru.siddalasha@capgemini.com>
2022-09-21 16:26:39 +0530

    Assertions Changes

--- src/test/java/com/ibm/sdwan/velocloud/utils/FileUtilsTest.java
+++ src/test/java/com/ibm/sdwan/velocloud/utils/FileUtilsTest.java
@@ -11,9 +11,15 @@ public class FileUtilsTest {
 	@Test
 	@DisplayName("Testing positive scenario for LifecycleScripts for filename and entry are same")
 	public void getFileFromLifecycleScriptsTest() {
+		String expected="{\n"
+				+ "  \"enterpriseId\": {{ enterpriseId }},\n"
+				+ "  \"id\": {{ id }}\n"
+				+ "}";
 		String result =FileUtils.getFileFromLifecycleScripts("UEsDBBQACAgIAAAAIQAAAAAAAAAAAAAAAAAZABEAdGVtcGxhdGVzL0RlbGV0ZUVkZ2UuanNvblVUDQAHAAAAAAAAAAAAAAAAq+ZSUFBKzStJLSooyixO9UxRslKorlZAFlGordUBqcqEymWCRLhqAVBLBwhH2pcgKAAAADoAAABQSwMEFAAICAgAAAAhAAAAAAAAAAAAAAAAABwAEQB0ZW1wbGF0ZXMvRWRnZVByb3Zpc2lvbi5qc29uVVQNAAcAAAAAAAAAAAAAAABdzkEKwyAQheF9TiGue4LuWyiU3GESX4OgY1HTUoJ3jxYNtkv/+ZhxG4SQ4Aj/9DrgpuRZbJvoi0jpVNTs+KGX1VPUjhv8i82SgY/hwjQZFCkz/WkZyq+0TsGMq53gq+vKoaAW3PUM7r7Yp3ZX4ZXTlaw2n7quT8c+Jos6D+pNPOZ3GQ5pB1BLBwhYrn36hgAAABEBAABQSwECFAAUAAgICAAAACEAR9qXICgAAAA6AAAAGQAJAAAAAAAAAAAAAAAAAAAAdGVtcGxhdGVzL0RlbGV0ZUVkZ2UuanNvblVUBQAHAAAAAFBLAQIUABQACAgIAAAAIQBYrn36hgAAABEBAAAcAAkAAAAAAAAAAAAAAIAAAAB0ZW1wbGF0ZXMvRWRnZVByb3Zpc2lvbi5qc29uVVQFAAcAAAAAUEsFBgAAAAACAAIAowAAAGEBAAAAAA==", 
 				 "templates/DeleteEdge.json");
 		Assertions.assertNotNull(result);
+		Assertions.assertEquals(expected, result);
+		
 		
 	}
 	
